bbae796c207b7f8d3248ea300a719c57eb01cb45
makitani <makitani007@icloud.com>
2015-08-23 14:06:26 +0900

    MyProductでログイン画面から地図画面への遷移を実装。

--- MyProduct/app/src/main/java/com/example/makitani/myproduct/LogInActivity.java
+++ MyProduct/app/src/main/java/com/example/makitani/myproduct/LogInActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -22,9 +24,14 @@ import org.json.JSONObject;
 import java.util.Arrays;
 import java.util.List;
 
+
+
+
 public class LoginActivity extends AppCompatActivity {
     private CallbackManager callbackManager;
     private LoginButton loginButton;
+
+//    private Button LogIn;
 
     @Override
     protected void onActivityResult(int requestCode, int responseCode, Intent data) {
@@ -41,10 +48,12 @@ public class LoginActivity extends AppCompatActivity {
         //LoginButtonはFacebookSDKに含まれたButtonウィジェットの拡張ウィジェット。
         loginButton = (LoginButton) findViewById(R.id.login_btn_facebook);
         //APIから取得したいユーザー情報の種類を指定し、パーミッションを許可する。
+
         List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "public_profile");
         loginButton.setReadPermissions(permissionNeeds);
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
             @Override
             public void onSuccess(LoginResult loginResult) {
                 System.out.println("");
@@ -52,7 +61,7 @@ public class LoginActivity extends AppCompatActivity {
                         (loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
                             @Override
                             public void onCompleted(JSONObject object, GraphResponse response) {
-                            //API連携に成功し、ログイン状態になったら、下記のようなゲッター関数により、ユーザー情報を取得することが出来る。
+                                //API連携に成功し、ログイン状態になったら、下記のようなゲッター関数により、ユーザー情報を取得することが出来る。
                                 try {
                                     String id = object.getString("id");
                                     String name = object.getString("name");
@@ -68,6 +77,28 @@ public class LoginActivity extends AppCompatActivity {
                 parameters.putString("fields", "id,name,email,gender, birthday");
                 request.setParameters(parameters);
                 request.executeAsync();
+
+                if("id"!=null){
+                    Intent intent = new Intent(LoginActivity.this, TopActivity.class);
+                    startActivity(intent);
+                }
+
+/*
+                loginButton.setOnClickListener(new View.OnClickListener(){
+
+                    public View.OnClickListener loginButton_ClickListener = new View.OnClickListener(){
+                        public void onClick(View v) {
+                            loginButton_Click();
+                        }
+                    };
+
+                    private void  loginButton_Click () {
+                        Intent intent = new Intent(this, TopActivity.class);
+                        startActivity(intent);
+                    }
+
+
+                });*/
             }
 
             @Override
@@ -84,68 +115,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-////FacebookSdkを使う場合、必須。Activityのレイアウトファイルを定義する箇所よりも前に記述する。
-//        FacebookSdk.sdkInitialize(this);
-//        setContentView(R.layout.activity_log_in);
-//    }
-
-    /*
-        Profile prof= Profile.getCurrentProfile();
-    // prof変数がnullでなければ、現在ログイン中であるということが分かる。
-        if(prof != null)
-        {
-            v("facebook_login", "1:" + prof.getName());
-            v("facebook_login", "1:" + prof.getFirstName());
-            v("facebook_login", "1:" + prof.getLastName());
-            v("facebook_login", "1:" + prof.getID());
-        } else {
-    //Prof変数がnullの場合、現在ログアウト中であることがわかる。
-            v("facebook_login","2");
-        }
-
-    public class LogInActivity extends AppCompatActivity {
-
-        private Button LogIn;
-
-        private View.OnClickListener LogIn_ClickListener = new View.OnClickListener(){
-            public void onClick(View v) {
-                LogIn_Click();
-            }
-        };
-
-
-        private void setViewObject () {
-
-            LogIn = (Button) findViewById(R.id.LogIn);
-        }
-
-        private void setListner(){
-            LogIn.setOnClickListener(LogIn_ClickListener);
-        }
-
-        private void  LogIn_Click () {
-            Intent intent = new Intent(this, TopActivity.class);
-            startActivity(intent);
-        }
-
-
-
-
-
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_log_in);
-
-            setViewObject();
-            setListner();
-
-        }
-    */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
